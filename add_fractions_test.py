bd0dac5be559884431080a4ac30cc58627455f3a
Thomas Traude <thomas.traude@codecentric.de>
2022-05-30 11:35:05 +0200

    handle 0 plus 0

---
+++ add_fractions_test.py
@@ -0,0 +1,38 @@
+class Fraction:
+
+    def __init__(self, integer_value) -> None:
+        super().__init__()
+
+    def plus(self, param):
+        return self
+
+    def int_value(self):
+        return 0
+
+
+def test_zero_plus_zero():
+    sum = Fraction(0).plus(Fraction(0))
+    assert sum.int_value() == 0
+
+
+"""
+0 + 0 = 0
+3 + 0 = 0
+0 + 5 = 0
+3 + 4 = 7
+
+-3 + 1 = -2
+
+1/5 + 2/5 = 3/5
+
+1/2 + 1/3 = 5/6
+
+3/4 + 5/8 = 11/8
+
+1/6 + 4/9 = 11/18
+
+-1/4 + 3/4 = 1/2
+3/8 + -1/2 = -1/8
+
+1/-4 + (-3/-4)  = 1/2
+"""
